76c85fdd72f2d10906cc413852cd64ee3a14fec6
Mergim <mergimberisha0@gmail.com>
2023-05-17 17:18:47 +0100

    specification os on the way

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -88,6 +88,22 @@ describe Application do
       expect(homepage_response.body).to include('London Mansion')
       expect(homepage_response.body).to include('A beautiful mansion right under the hollywood sign in London')
     end
+
+    it 'confirms the listing that has been posted with dates' do
+      response = post(
+        '/new_listing',
+        title: 'London Mansion', 
+        description: 'A beautiful mansion right under the hollywood sign in London', 
+        price: 100.00,
+        img: 'https://img.gtsstatic.net/reno/imagereader.aspx?imageurl=https%3A%2F%2Fapi-prod.corelogic.com%2Ftrestle%2FMedia%2FCRMLS%2FProperty%2FPHOTO-jpeg%2F1013309050%2F1%2FMTU3Mi8yMjU2LzE5%2FMTkvODYyMy8xNjY2MzU5MzMy%2F_6mUwpQWQj53sOa1IBCE020pr-hhR0aS3syDBif_gPc%3Fdate%3D2023-02-15&option=N&h=472&permitphotoenlargement=false',
+        location: 'London',
+        user_id: 1,
+        date_3: 'true'
+        )
+
+       expect(response.status).to eq 200 
+
+    end
   end
   
   context 'POST /booking/:id' do
@@ -102,8 +118,11 @@ describe Application do
 
   context 'POST /listing/:id' do 
     it 'sends the booking request information to the server' do
-       
+      response = post('/listing/1', name: 'john smith', comment: 'I want to stay here', date_3: 'true')
 
+      expect(response.status).to eq 200
+      expect(response.body).to include('Booking successfully sent')
+      expect(response.body).to include('You have booked Hollywood Mansion for 3rd August')
     end
   end
 end
